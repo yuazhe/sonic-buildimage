c3f332f997836fffaaa31dd1b07b0d2615b69e4b
Volodymyr Samotiy <volodymyrs@nvidia.com>
2025-09-17 10:09:17 +0300

    [master_RC] Update SimX version to 25.7-1097

--- platform/mellanox/fw.mk
+++ platform/mellanox/fw.mk
@@ -21,7 +21,7 @@ MLNX_FW_BASE_PATH = $(MLNX_SDK_BASE_PATH)
 # Place an URL here to FW if you want to download FW instead
 MLNX_FW_BASE_URL =
 
-SIMX_VERSION = 25.7-1093
+SIMX_VERSION = 25.7-1097
 
 FW_FROM_URL = y
 
